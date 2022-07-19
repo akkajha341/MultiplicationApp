c3a3f761671671e457d3987d05cfa700a211924f
Aakanksha Jha <akkajha341@gmail.com>
2022-07-19 23:12:57 +0530

    second

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const num2=Math.ceil(Math.random()*10);
 const questionEl=document.getElementById("question")
 const formEl=document.getElementById("form")
 const inputEl=document.getElementById("input")
+const scoreEl=document.getElementById("score")
 
-let score=0
+let score=JSON.parse(localStorage.getItem("score"))
+if(!score)
+{
+   score=0
+}
+
+scoreEl.innerText= `score: ${score}`
 questionEl.innerText = `What is ${num1} multiply by ${num2}?`
 
 
