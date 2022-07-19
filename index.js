d4b76a5850173c0da3a1580a34bced6db636ee5e
Aakanksha Jha <akkajha341@gmail.com>
2022-07-19 22:44:18 +0530

    changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const num2=Math.ceil(Math.random()*10);
 const questionEl=document.getElementById("question")
 const formEl=document.getElementById("form")
 const inputEl=document.getElementById("input")
+
 let score=0
 questionEl.innerText = `What is ${num1} multiply by ${num2}?`
 
@@ -12,6 +13,22 @@ questionEl.innerText = `What is ${num1} multiply by ${num2}?`
 const correctAns=num1*num2
 
 formEl.addEventListener("submit",()=>{
-   const userAns=inputEl.value 
+   const userAns=+inputEl.value 
    console.log(userAns)
-})
+   if(userAns === correctAns)
+   {
+      score++;
+      updateLocalStorage()
+   }
+   else
+   {
+      score--;
+      updateLocalStorage()
+   }
+
+});
+
+function updateLocalStorage()
+{
+   localStorage.setItem("score",JSON.stringify(score));
+}
